8d571721f059f65bc4e1389fc3ca433d1a29d5dd
Cindy <104218618+CindyChan48@users.noreply.github.com>
2023-07-15 02:05:51 +0000

    modify

--- lesson6-1.py
+++ lesson6-1.py
@@ -1,8 +1,50 @@
-#!/usr/bin/python3.10.2
-
-import random
-
-min=1
-max=100
-target =random.randint(min,max)
-print(target)
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "44\n"
+     ]
+    }
+   ],
+   "source": [
+    "#!/usr/bin/python3.10.2\n",
+    "\n",
+    "import random\n",
+    "\n",
+    "min=1\n",
+    "max=100\n",
+    "target =random.randint(min,max)\n",
+    "print(target)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.8"
+  },
+  "orig_nbformat": 4
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
